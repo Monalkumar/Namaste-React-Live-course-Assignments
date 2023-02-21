6a67ed735e5e688e0d24984da25e7f87790845b4
MonalKumar <monalshiv123@gmail.com>
2023-02-22 02:19:20 +0530

    again commit

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,13 +1,10 @@
 import React from "react";
-import { Image, Shimmer } from 'react-shimmer'
+
 
 const Shimmer=()=>{
     return(
         <div>
-      <Image
-        src='https://source.unsplash.com/random/800x600'
-        fallback={<Shimmer width={500} height={600} />}
-      />
+      <h1>hello</h1>
     </div>
     )
 }
